28ead60a369beee30cb2bd75385dadba40defc10
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-04-23 17:42:45 +0300

    add credential account class to manage users account

--- pswdmanage.py
+++ pswdmanage.py
@@ -3,7 +3,7 @@ import pyperclip
 import random
 import string
 
-# class to create user login account
+# first class to create user login account
 class User:
     """
     User class will generates new object/instances of User.
@@ -28,21 +28,89 @@ class User:
         return cls.user_list
     def delete_user(self):
         '''
-        delete_account method deletes a  saved account from the list
+        This method uses .remove() mthd to delete an already saved account from the user_list
         '''
         User.user_list.remove(self)
     def user_detail(self):
         print(self.username)
         print(self.password)
 
-def main():
+def main(): #This function will not be called when other modules are ran
     new_user = User (username="annet",password="annet254") 
     new_user.user_detail() 
 
-if __name__=="__main__":
+if __name__=="__main__": #only up one running this file will the line 38-40 be executed
     main() 
 
-   
+# second class to handle users credential accounts
+class Credentials():
+    """
+    Credentials class will generates new object/instances of Credentials
+    """
+    credentials_list = [] #list to append users crediantial accounts
+    @classmethod
+    def validate_user(cls,username, password):
+        """
+        This method to validate whether the user is in our user_list or not.
+        """
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                    a_user == user.username
+        return a_user
+
+    def __init__(self, account, userName, password):
+        """
+         This method defines the properties needed when instanciating new account Credentials.
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password    
+    def save_details(self):
+        """
+        This method appends new account credential to the credentials_list.
+        """
+        Credentials.credentials_list.append(self) 
+    @classmethod
+    def search_credential(cls, account):
+        """
+        This method takes account_name as parameter and returns a credential that matches that account_name.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
+    def delete_credentials(self):
+        """
+        This method uses .remove() mthd to delete one account credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)            
+    @classmethod
+    def copy_password(cls,account):
+        found_credentials = Credentials.find_credential(account)
+        pyperclip.copy(found_credentials.password)
+
+    @classmethod
+    def if_credential_exist(cls, account):
+        """
+        This method confirms if a credential exists from the credential list
+         and returns True or False depending on credential existence.
+        """
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return True
+        return False
+    @classmethod
+    def display_credentials(cls):
+        """
+        This method returns all list items(credential accounts) in the credentials list
+        """
+        return cls.credentials_list[0,8]
+
+    def generatePassword(stringLength=8):
+        """This method will generate random password with letters,
+            digits and special char list symbols"""
+        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+        return ''.join(random.choice(password) for i in range(stringLength))
 
 
 
--- test_pswdmanage.py
+++ test_pswdmanage.py
@@ -1,3 +1,4 @@
 from pswdmanage import User
+from pswdmanage import Credentials
 import unittest
 # import our class from pswdmanage module
